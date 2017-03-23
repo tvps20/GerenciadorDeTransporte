b01afa8cba8fc07690bfd36863e552f84969f284
Thiago Santiago <t_vps20@gmail.com>
2017-03-23 08:42:59 -0300

    Novas opções adicionadas

--- GerenciadorDeTransporte/src/gerenciadordetransporte/Menu.java
+++ GerenciadorDeTransporte/src/gerenciadordetransporte/Menu.java
@@ -8,6 +8,9 @@ public class Menu
         System.out.println("1 - Cadastrar");
         System.out.println("2 - Ver Casdatros");
         System.out.println("3 - Tabela de Horarios da semana");
+        System.out.println("4 - Sair");
+        System.out.println();
+        System.out.println("Digite sua Escolha: ");
     }
     
     public void menuDeCadastro()
@@ -17,15 +20,21 @@ public class Menu
         System.out.println("2 - Cadastrar Paciente");
         System.out.println("3 - Cadastrar Motorista");
         System.out.println("5 - Cadastrar Veículo");
+        System.out.println("6 - Voltar");
+        System.out.println();
+        System.out.println("Digite sua Escolha: ");
     }
     
-    public void menuDeCadastrados()
+    public void menuDeExibicao()
     {
         System.out.println("======= Ver Cadastros =======");
         System.out.println("1 - Alunos");
         System.out.println("2 - Pacientes");
         System.out.println("3 - Motoristas");
         System.out.println("5 - Veículos");
+        System.out.println("6 - Voltar");
+        System.out.println();
+        System.out.println("Digite sua Escolha: ");
     }
     
 }
