30f51ee9f1e034a9259071ff12c53c6a6395f6c5
Thiago Santiago <t_vps20@gmail.com>
2017-03-23 08:45:03 -0300

    Começando a criar a logica de funcionamento do menu

--- GerenciadorDeTransporte/src/gerenciadordetransporte/RodarPrograma.java
+++ GerenciadorDeTransporte/src/gerenciadordetransporte/RodarPrograma.java
@@ -1,9 +1,48 @@
 package gerenciadordetransporte;
 
+import static java.awt.SystemColor.menu;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class RodarPrograma 
 {
+    static Menu menu = new Menu();
+    
     public static void main(String[] args) 
-    {        
+    {  
+        boolean loop = true;
         
+        do
+        {
+            try
+            {
+                Scanner entrada = new Scanner(System.in);
+                
+                menu.menuPrincipal();
+                int escolha = entrada.nextInt();
+                
+                switch(escolha)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        loop = false;
+                        break;
+                    default:
+                        System.out.println("Opção Inválida!");
+                        System.out.println();
+                }
+            }
+            catch(InputMismatchException e)
+            {
+               System.out.println("Digite uma opção válida"); 
+               System.out.println();
+            }
+            
+        }while(loop);
     }   
 }
