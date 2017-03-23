a4e4af67fbd81618827f3ce3ce62d9aa599f0ff6
Thiago Santiago <t_vps20@gmail.com>
2017-03-23 08:26:31 -0300

    Criado os metodos para chamar os menus

--- GerenciadorDeTransporte/src/gerenciadordetransporte/Menu.java
+++ GerenciadorDeTransporte/src/gerenciadordetransporte/Menu.java
@@ -1,5 +1,31 @@
 package gerenciadordetransporte;
 
-public class Menu {
+public class Menu 
+{
+    public void menuPrincipal()
+    {
+        System.out.println("======= Menu Principal =======");
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Ver Casdatros");
+        System.out.println("3 - Tabela de Horarios da semana");
+    }
+    
+    public void menuDeCadastro()
+    {
+        System.out.println("======= Cadastrar =======");
+        System.out.println("1 - Cadastrar Aluno");
+        System.out.println("2 - Cadastrar Paciente");
+        System.out.println("3 - Cadastrar Motorista");
+        System.out.println("5 - Cadastrar Veículo");
+    }
+    
+    public void menuDeCadastrados()
+    {
+        System.out.println("======= Ver Cadastros =======");
+        System.out.println("1 - Alunos");
+        System.out.println("2 - Pacientes");
+        System.out.println("3 - Motoristas");
+        System.out.println("5 - Veículos");
+    }
     
 }
