715e3888ebf34e7e5579af888b60572188b472f0
Thiago Santiago <t_vps20@gmail.com>
2017-03-23 08:51:29 -0300

    Comentários adicionados

--- GerenciadorDeTransporte/src/gerenciadordetransporte/RodarPrograma.java
+++ GerenciadorDeTransporte/src/gerenciadordetransporte/RodarPrograma.java
@@ -11,16 +11,18 @@ public class RodarPrograma
     public static void main(String[] args) 
     {  
         boolean loop = true;
-        
+        //Garantindo que o loop acontece pelo menos uma vez, por isso o uso do doWhile.
         do
         {
+            //Tratar os erros de digitação que sejam diferentes de inteiros.
             try
             {
+                //Instanciando a classe Scanner para capturar a entrada informada pelo usuario.
                 Scanner entrada = new Scanner(System.in);
-                
+                //Chamando o menu principal e salvando a entrada do usuario.
                 menu.menuPrincipal();
                 int escolha = entrada.nextInt();
-                
+                //Usando o switch para chamar os metodos referentes a escolha solicitada.
                 switch(escolha)
                 {
                     case 1:
@@ -33,12 +35,14 @@ public class RodarPrograma
                         loop = false;
                         break;
                     default:
+                        //Informando ao usuário que sua escolha é inválida.
                         System.out.println("Opção Inválida!");
                         System.out.println();
                 }
             }
             catch(InputMismatchException e)
             {
+               //Informando ao usuário que sua escolha é inválida.
                System.out.println("Digite uma opção válida"); 
                System.out.println();
             }
