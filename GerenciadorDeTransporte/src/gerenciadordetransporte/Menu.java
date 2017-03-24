6a875336ef78a0e894dbdc04053b23a27f7172e2
Thiago Santiago <t_vps20@gmail.com>
2017-03-23 21:05:41 -0300

    Modificando o Menu

--- GerenciadorDeTransporte/src/gerenciadordetransporte/Menu.java
+++ GerenciadorDeTransporte/src/gerenciadordetransporte/Menu.java
@@ -5,36 +5,52 @@ public class Menu
     public void menuPrincipal()
     {
         System.out.println("======= Menu Principal =======");
-        System.out.println("1 - Cadastrar");
-        System.out.println("2 - Ver Casdatros");
-        System.out.println("3 - Tabela de Horarios da semana");
-        System.out.println("4 - Sair");
+        System.out.println("1 - Gerenciar Educacao");
+        System.out.println("2 - Gerenciar Saude");
+        System.out.println("3 - Gerenciar Transporte");
+        System.out.println("4 - Tabela De Horarios");
+        System.out.println("5 - Sair");
         System.out.println();
         System.out.println("Digite sua Escolha: ");
     }
     
-    public void menuDeCadastro()
+    public void menuDeEducacao()
     {
-        System.out.println("======= Cadastrar =======");
+        System.out.println("======= Gerenciar Educacao =======");
         System.out.println("1 - Cadastrar Aluno");
-        System.out.println("2 - Cadastrar Paciente");
-        System.out.println("3 - Cadastrar Motorista");
-        System.out.println("4 - Cadastrar Veículo");
-        System.out.println("5 - Voltar");
+        System.out.println("2 - Cadastrar Viagem");
+        System.out.println("3 - Ver Alunos");
+        System.out.println("4 - Ver Viagens");
         System.out.println();
         System.out.println("Digite sua Escolha: ");
     }
     
-    public void menuDeExibicao()
+    public void menuDeSaude()
     {
-        System.out.println("======= Ver Cadastros =======");
-        System.out.println("1 - Alunos");
-        System.out.println("2 - Pacientes");
-        System.out.println("3 - Motoristas");
-        System.out.println("4 - Veículos");
-        System.out.println("5 - Voltar");
+        System.out.println("======= Gerenciar Saude =======");
+        System.out.println("1 - Cadastrar Paciente");
+        System.out.println("2 - Cadastrar Viagem");
+        System.out.println("3 - Ver Pacientes");
+        System.out.println("4 - Ver Alunos");
         System.out.println();
         System.out.println("Digite sua Escolha: ");
+    }
+    
+    public void menuDeTransporte()
+    {
+        System.out.println("======= Gerenciar Transporte =======");
+        System.out.println("1 - Cadastrar Motorista");
+        System.out.println("2 - Cadastrar Veiculos");
+        System.out.println("3 - Ver Motoristas");
+        System.out.println("4 - Ver Veículos");
+        System.out.println();
+        System.out.println("Digite sua Escolha: ");
+    }
+    
+    public void TabelaDeHorarios()
+    {
+        System.out.println("======= Tabela de Horarios =======");
+        System.out.println();
     }
     
 }
