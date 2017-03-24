c0538f4644f5ed90509fcd3f59ed6b9837abfbe6
Thiago Santiago <t_vps20@gmail.com>
2017-03-23 22:00:08 -0300

    Criando as classe de navegacao para os submenus

---
+++ GerenciadorDeTransporte/src/Global/NavegacaoSaude.java
@@ -0,0 +1,28 @@
+package Global;
+
+import Interfaces.INavegacaoSaude;
+
+public class NavegacaoSaude extends Navegacao implements INavegacaoSaude
+{
+
+    @Override
+    public int irParaCadastrarPaciente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int irParaCadastrarViagem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int irParaVerPacientes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int irPAraVerViagens() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ GerenciadorDeTransporte/src/Global/NavegacaoTransporte.java
@@ -0,0 +1,28 @@
+package Global;
+
+import Interfaces.INavegacaoTransporte;
+
+public class NavegacaoTransporte extends Navegacao implements INavegacaoTransporte
+{
+
+    @Override
+    public int irParaCadastrarMotorista() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int irParaCadastarVeiculo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int irParaVerMotoristas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int irParaVerVeiculos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
