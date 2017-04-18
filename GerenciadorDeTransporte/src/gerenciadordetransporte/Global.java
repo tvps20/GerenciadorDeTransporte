747011196ca8496174533fb8a1a1e59dcb2d0397
Filipe Santiago <porto.santiago@hotmail.com>
2017-04-18 15:39:10 -0300

    Criada a função para mostrar as viagens

--- GerenciadorDeTransporte/src/gerenciadordetransporte/Global.java
+++ GerenciadorDeTransporte/src/gerenciadordetransporte/Global.java
@@ -10,16 +10,16 @@ public class Global
 {
     private static Scanner pegaEntrada = new Scanner(System.in);
     private static LinkedList<Aluno> listaDeAlunos = new LinkedList(); 
-    private static LinkedList<Viagem> ListaDeViagens = new LinkedList();
+    private static LinkedList<Viagem> listaDeViagens = new LinkedList();
     
  
     public static void AdicionarAluno()
     {
         Aluno novoAluno = new Aluno();
-        System.out.println("Digite o nome do Aluno: ");
+        System.out.println("Nome: ");
         String entrada = pegaEntrada.nextLine();
         novoAluno.setNome(entrada);
-        System.out.println("Digite a Instituição: ");
+        System.out.println("Instituição: ");
         entrada = pegaEntrada.nextLine();
         novoAluno.setInstituicao(entrada);
         listaDeAlunos.add(novoAluno);
@@ -29,13 +29,13 @@ public class Global
     
     public static void MostrarAlunos()
     {
-        Aluno[] alunos = new Aluno[listaDeAlunos.size()];
-        Aluno[] array = listaDeAlunos.toArray(alunos);
+        Aluno[] array = new Aluno[listaDeAlunos.size()];
+        Aluno[] alunos = listaDeAlunos.toArray(array);
         System.out.println("======= Alunos Cadastrados =======");
         
-        if(array.length > 0)
+        if(alunos.length > 0)
         {
-            for(Aluno atual: array)
+            for(Aluno atual: alunos)
                 System.out.println(atual.getNome() + " => " + atual.getInstituicao());
         }
         else
@@ -43,5 +43,40 @@ public class Global
         
         System.out.println();
         
+    }
+    
+    public static void AdicionarViagem()
+    {
+        Viagem novaViagem = new Viagem();
+        System.out.println("Destino: ");
+        String entrada = pegaEntrada.nextLine();
+        novaViagem.setDestino(entrada);
+        System.out.println("Horario: ");
+        entrada = pegaEntrada.nextLine();
+        novaViagem.setHorario(entrada);
+        System.out.println("Turno: ");
+        entrada = pegaEntrada.nextLine();
+        novaViagem.setTurno(entrada);
+        listaDeViagens.add(novaViagem);
+        System.out.println("Viagem Adicionada");
+        System.out.println();
+        
+    }
+    
+    public static void MostrarViagens()
+    {
+        Viagem[] array = new Viagem[listaDeViagens.size()];
+        Viagem[] viagens = listaDeViagens.toArray(array);
+        System.out.println("======= Viagens Cadastradas =======");
+        
+        if(viagens.length > 0)
+        {
+            for(Viagem atual: viagens)
+                System.out.println(atual.getDestino() + " => " + atual.getHorario() + " => " + atual.getTurno());
+        }
+        else
+            System.out.println("Nenhuma Viagem Cadastrada");
+        
+        System.out.println();
     }
 }
