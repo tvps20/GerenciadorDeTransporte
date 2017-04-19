f92cb502ec78e46bce8d41fbde5670f7eefa0853
Thiago Santiago <t_vps20@gmail.com>
2017-04-19 13:24:36 -0300

    Criando as funções para cadastrar as viagens da saude

---
+++ GerenciadorDeTransporte/src/GerenciarTransporte/GlobalSaude.java
@@ -0,0 +1,15 @@
+package GerenciarTransporte;
+
+import GerenciarTransporte.Cadastros.Paciente;
+import GerenciarTransporte.Cadastros.Viagem;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
+public class GlobalSaude 
+{
+    private static Scanner pegaEntrada = new Scanner(System.in);
+    private static LinkedList<Paciente> listaDePacientes = new LinkedList();
+    private static LinkedList<Viagem> listaDeViagens = new LinkedList();
+    
+}
