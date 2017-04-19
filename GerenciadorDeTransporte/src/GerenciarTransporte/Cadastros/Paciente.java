713601d3dd1d28bc9e78347601590481f030b199
Thiago Santiago <t_vps20@gmail.com>
2017-04-19 13:24:09 -0300

    Criando a classe Paciente

---
+++ GerenciadorDeTransporte/src/GerenciarTransporte/Cadastros/Paciente.java
@@ -0,0 +1,23 @@
+package GerenciarTransporte.Cadastros;
+
+public class Paciente 
+{
+    private String Nome;
+    private String Hospital;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getHospital() {
+        return Hospital;
+    }
+
+    public void setHospital(String Hospital) {
+        this.Hospital = Hospital;
+    }
+}
