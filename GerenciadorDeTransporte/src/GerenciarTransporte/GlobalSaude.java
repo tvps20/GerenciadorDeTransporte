e51042df6412dce30e88f318c5850957bc9521e2
Thiago Santiago <t_vps20@gmail.com>
2017-04-19 16:51:27 -0300

    Funções para cadastro e mostra pacientes implementadas

--- GerenciadorDeTransporte/src/GerenciarTransporte/GlobalSaude.java
+++ GerenciadorDeTransporte/src/GerenciarTransporte/GlobalSaude.java
@@ -12,4 +12,34 @@ public class GlobalSaude
     private static LinkedList<Paciente> listaDePacientes = new LinkedList();
     private static LinkedList<Viagem> listaDeViagens = new LinkedList();
     
+    public static void AdicionarPaciente()
+    {
+        Paciente novoPaciente = new Paciente();
+        System.out.println("Nome: ");
+        String entrada = pegaEntrada.nextLine();
+        novoPaciente.setNome(entrada);
+        System.out.println("Destino: ");
+        entrada = pegaEntrada.nextLine();
+        novoPaciente.setDestino(entrada);
+        listaDePacientes.add(novoPaciente);
+        System.out.println("Paciente Adicionado");
+        System.out.println();
+    }
+    
+    public static void MostarPacientes()
+    {
+        Paciente[] array = new Paciente[listaDePacientes.size()];
+        Paciente[] pacientes = listaDePacientes.toArray(array);
+        System.out.println("======= Pacientes Cadastrados =======");
+        
+        if(pacientes.length > 0)
+        {
+            for(Paciente atual: pacientes)
+                System.out.println(atual.getNome() + " => " + atual.getDestino());
+        }
+        else
+            System.out.println("Nenhum Paciente Encontrado.");
+        
+        System.out.println();
+    }
 }
