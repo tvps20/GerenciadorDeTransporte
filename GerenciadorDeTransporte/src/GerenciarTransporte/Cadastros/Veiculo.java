6d534ca66d718cb2289ddba718f7678690dedea8
Thiago Santiago <t_vps20@gmail.com>
2017-04-20 19:23:40 -0300

    Adicionando os atributos na classe veiculo

--- GerenciadorDeTransporte/src/GerenciarTransporte/Cadastros/Veiculo.java
+++ GerenciadorDeTransporte/src/GerenciarTransporte/Cadastros/Veiculo.java
@@ -2,5 +2,23 @@ package GerenciarTransporte.Cadastros;
 
 public class Veiculo 
 {
+    private String modelo;
+    private String placa;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
     
 }
