2c32e88d778afe59018f3a33732b78f6f7fc5aaf
Thiago Santiago <t_vps20@gmail.com>
2017-04-21 09:57:57 -0300

    Adicionando novo campo a classe viagem

--- GerenciadorDeTransporte/src/GerenciarTransporte/Cadastros/Viagem.java
+++ GerenciadorDeTransporte/src/GerenciarTransporte/Cadastros/Viagem.java
@@ -5,38 +5,47 @@ public class Viagem
     private String Destino;
     private String horario;
     private String turno;
+    private String dia;
     private Paciente paciente;
     private String acompanhante;
     
-
 
     public String getDestino() {
         return Destino;
     }
 
-
     public void setDestino(String Destino) {
         this.Destino = Destino;
     }
 
+    
     public String getHorario() {
         return horario;
     }
 
-
     public void setHorario(String horario) {
         this.horario = horario;
     }
+
     
     public String getTurno() {
         return turno;
     }
 
-
     public void setTurno(String turno) {
         this.turno = turno;
     }
+    
+    
+    public String getDia() {
+        return dia;
+    }
 
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    
     public Paciente getPaciente() {
         return paciente;
     }
@@ -45,6 +54,7 @@ public class Viagem
         this.paciente = paciente;
     }
 
+    
     public String getAcompanhante() {
         return acompanhante;
     }
