1d8550ac1fd33383b53dfc58beae9a0e4410fd8c
Thiago Santiago <t_vps20@gmail.com>
2017-04-21 12:43:09 -0300

    Criada a classe global de funções para tabela de horario

--- GerenciadorDeTransporte/src/GerenciarTransporte/GlobalTabelaDeHorarios.java
+++ GerenciadorDeTransporte/src/GerenciarTransporte/GlobalTabelaDeHorarios.java
@@ -1,10 +1,38 @@
 package GerenciarTransporte;
 
+import GerenciarTransporte.Cadastros.Viagem;
+import GerenciarTransporte.Enumeracao.Dias;
+import static GerenciarTransporte.GlobalEducacao.*;
+import static GerenciarTransporte.GlobalSaude.*;
+
 public class GlobalTabelaDeHorarios 
-{
+{    
     public static void MostarTabelaDeHorarios()
     {
-        System.out.println();
-    }
-    
+        Viagem[] educacao = ViagensEducacao();
+        Viagem[] saude = ViagensSaude();
+        
+        if((educacao.length > 0) && (saude.length > 0))
+        {
+            System.out.println("#### Educacao ####");
+            for(Viagem atual: educacao)
+                System.out.println(atual.getDestino() + " => " + atual.getDias() +" => " + atual.getHorario() 
+                        + " => " + atual.getTurno());
+            System.out.println();
+            System.out.println("#### Saude ####");
+            for(Viagem atual: saude)
+            {
+                System.out.println("Destino: " + atual.getDestino());
+                System.out.println("Dia: " + atual.getDias()+ " => " + atual.getHorario() + " => " + atual.getTurno()); 
+                System.out.println("Paciente: " + atual.getPaciente().getNome()); 
+                System.out.println("Acompanhante: " + atual.getAcompanhante());
+                System.out.println();
+            }
+            System.out.println();
+        }
+        else
+            System.out.println("Nenhuma viagem cadastrada.");
+        
+        System.out.println();     
+    }    
 }
